63ae64fa600d36f162ea93a809bf58014458a005
Areeb Shahid <areebshahid85@gmail.com>
2023-12-13 21:32:09 +0800

    Added supabase

--- connectofrontend/lib/main.dart
+++ connectofrontend/lib/main.dart
@@ -1,10 +1,21 @@
 import 'dart:io';
-
 import 'package:connectofrontend/screens/onboarding/onboarding.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://iuiwtlozrllebllvbqal.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1aXd0bG96cmxsZWJsbHZicWFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI0NTkwNjEsImV4cCI6MjAxODAzNTA2MX0.LsXfZ8gFvqOzz2szQ0Ld0t0fuJ8oXP6xyUSiDZAFB0M',
+  );
+
+  final supabase = Supabase.instance.client;
+  final response = await supabase.from('User_Table').upsert([
+    {'Id': 85432644, 'Name': 'Test'},
+  ]);
+
   runApp(const MyApp());
 }
 
