89d9704e7ba4fd8fcba692e60bf4b4ff2885c2a4
ish1506 <ishiuanlee@gmail.com>
2023-12-21 21:50:40 +0800

    Implement add device functionality
    - Does not include bluetooth pairing steps

--- connectofrontend/lib/widgets/home_system_rooms_tab.dart
+++ connectofrontend/lib/widgets/home_system_rooms_tab.dart
@@ -2,20 +2,39 @@ import 'package:connectofrontend/models/room.dart';
 import 'package:connectofrontend/widgets/room_devices_tab.dart';
 import 'package:flutter/material.dart';
 
-class HomeSystemRoomsTab extends StatelessWidget {
+class HomeSystemRoomsTab extends StatefulWidget {
   final List<Room> rooms;
 
   const HomeSystemRoomsTab({super.key, required this.rooms});
 
   @override
+  State<HomeSystemRoomsTab> createState() => _HomeSystemRoomsTabState();
+}
+
+class _HomeSystemRoomsTabState extends State<HomeSystemRoomsTab> {
+  void handleRoomAdded(Room newRoom) {
+    setState(() {
+      widget.rooms.add(newRoom);
+    });
+  }
+
+  void handleRoomUpdated(Room newRoom) {
+    setState(() {
+      Room room = widget.rooms.firstWhere((r) => r.name == newRoom.name);
+      room.updateRoomDevices(newRoom.devices);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Row(
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: rooms
+      children: widget.rooms
           .map(
             (room) => Container(
               margin: const EdgeInsets.symmetric(horizontal: 8),
-              child: RoomDevicesTab(room: room),
+              child:
+                  RoomDevicesTab(room: room, onRoomUpdated: handleRoomUpdated),
             ),
           )
           .toList(),
--- connectofrontend/lib/widgets/room_devices_tab.dart
+++ connectofrontend/lib/widgets/room_devices_tab.dart
@@ -1,4 +1,5 @@
 import 'package:connectofrontend/models/device/device.dart';
+import 'package:connectofrontend/models/device/fan_device.dart';
 import 'package:connectofrontend/models/device/light_device.dart';
 import 'package:connectofrontend/models/room.dart';
 import 'package:connectofrontend/widgets/custom_switch.dart';
@@ -6,10 +7,12 @@ import 'package:flutter/material.dart';
 
 class RoomDevicesTab extends StatefulWidget {
   final Room room;
+  final Function(Room) onRoomUpdated;
 
   const RoomDevicesTab({
     super.key,
     required this.room,
+    required this.onRoomUpdated,
   });
 
   @override
@@ -17,6 +20,76 @@ class RoomDevicesTab extends StatefulWidget {
 }
 
 class _RoomDevicesTabState extends State<RoomDevicesTab> {
+  String selectedDeviceType = 'Fan';
+  TextEditingController deviceNameController = TextEditingController();
+
+  Future<void> _showAddDeviceDialog(BuildContext context, Room room) async {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Add Device'),
+          content: Column(
+            children: [
+              TextField(
+                controller: deviceNameController,
+                decoration: const InputDecoration(labelText: 'Device Name'),
+              ),
+              DropdownButton<String>(
+                value: selectedDeviceType,
+                items: <String>['Fan', 'Light'].map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (newValue) {
+                  setState(() {
+                    selectedDeviceType = newValue!;
+                  });
+                },
+              ),
+              TextField(
+                controller: TextEditingController()..text = widget.room.name,
+                decoration: const InputDecoration(
+                  labelText: 'Room',
+                ),
+                readOnly: true,
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                Device newDevice;
+                if (selectedDeviceType == 'Fan') {
+                  newDevice = FanDevice(name: deviceNameController.text);
+                } else if (selectedDeviceType == 'Light') {
+                  newDevice = LightDevice(name: deviceNameController.text);
+                } else {
+                  throw Exception('Invalid device type');
+                }
+
+                widget.room.addDevice(newDevice);
+                widget.onRoomUpdated(widget.room);
+                deviceNameController.clear();
+
+                Navigator.of(context).pop();
+              },
+              child: const Text('Add'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -50,6 +123,25 @@ class _RoomDevicesTabState extends State<RoomDevicesTab> {
             ...widget.room.devices
                 .map((device) => DeviceTab(device: device))
                 .toList(),
+            // TODO: Fix styling issue -- align to the left
+            GestureDetector(
+              onTap: () => _showAddDeviceDialog(context, widget.room),
+              child: const Align(
+                // * Doesn't work!?
+                // alignment: Alignment.centerLeft,
+                child: Row(
+                  // crossAxisAlignment: CrossAxisAlignment.start,
+                  // mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Icon(Icons.add_circle_outline),
+                    Text(
+                      'Add Device',
+                      // style: TextStyle(color: Colors.blue),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
