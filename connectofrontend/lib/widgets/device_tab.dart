f8a789891627a30e7f2459cef7351c1d7355234b
ish1506 <ishiuanlee@gmail.com>
2024-01-13 15:52:46 +0800

    Extract DeviceTab widget into its own file

---
+++ connectofrontend/lib/widgets/device_tab.dart
@@ -0,0 +1,135 @@
+import 'package:connectofrontend/models/device/device.dart';
+import 'package:connectofrontend/models/device/light_device.dart';
+import 'package:connectofrontend/widgets/custom_switch.dart';
+import 'package:connectofrontend/widgets/device_settings_menu.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class DeviceTab extends StatefulWidget {
+  final Device device;
+  final Function(Device) onDeviceUpdated;
+  final Function(Device) onDeviceDeleted;
+
+  const DeviceTab({
+    super.key,
+    required this.device,
+    required this.onDeviceUpdated,
+    required this.onDeviceDeleted,
+  });
+
+  @override
+  State<DeviceTab> createState() => _DeviceTabState();
+}
+
+class _DeviceTabState extends State<DeviceTab> {
+  // Change status of device when switch is toggled
+  void updateSwitch(SwitchStatus status) {
+    setState(() {
+      widget.device.isOn = status == SwitchStatus.on;
+    });
+
+    // OR once the state changes, go to main dashboard and trigger a function that checks if all devices are off
+    // pass the change to main dashboard
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String deviceIcon = widget.device is LightDevice
+        ? 'assets/icons/lightbulb.svg'
+        : 'assets/icons/fan.svg';
+
+    String deviceFeature =
+        widget.device is LightDevice ? 'Brightness' : 'Fan Speed';
+
+    double devicePercentage = widget.device.value;
+
+    return Container(
+      margin: const EdgeInsets.only(bottom: 24),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(8, 24, 32, 24),
+        child: Row(
+          children: [
+            DeviceSettingsMenu(
+              device: widget.device,
+              onDeviceUpdated: widget.onDeviceUpdated,
+              onDeviceDeleted: widget.onDeviceDeleted,
+            ),
+            Expanded(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          SvgPicture.asset(
+                            deviceIcon,
+                            height: 40,
+                            width: 40,
+                          ),
+                          const SizedBox(
+                            width: 16,
+                          ),
+                          Text(
+                            widget.device.name,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24,
+                            ),
+                          ),
+                        ],
+                      ),
+                      CustomSwitch(
+                        value: widget.device.isOn
+                            ? SwitchStatus.on
+                            : SwitchStatus.off,
+                        onChanged: updateSwitch,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        deviceFeature,
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                      Text(
+                        '${(devicePercentage * 100).round()}%',
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                      // Text(widget.device is LightDevice ? widget.device. : )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Slider(
+                    value: widget.device.value,
+                    min: 0,
+                    max: 1,
+                    // TODO: Persist the new value, communicate with IoT device
+                    onChanged: (newValue) {
+                      setState(() {
+                        widget.device.setValue(newValue);
+                      });
+                    },
+                    activeColor: const Color(0xFF455A64),
+                    inactiveColor: const Color(0xFFCFD8DC),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- connectofrontend/lib/widgets/room_devices_tab.dart
+++ connectofrontend/lib/widgets/room_devices_tab.dart
@@ -2,11 +2,9 @@ import 'package:connectofrontend/models/device/device.dart';
 import 'package:connectofrontend/models/device/fan_device.dart';
 import 'package:connectofrontend/models/device/light_device.dart';
 import 'package:connectofrontend/models/room.dart';
-import 'package:connectofrontend/widgets/custom_switch.dart';
-import 'package:connectofrontend/widgets/device_settings_menu.dart';
+import 'package:connectofrontend/widgets/device_tab.dart';
 import 'package:connectofrontend/widgets/room_settings_menu.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class RoomDevicesTab extends StatefulWidget {
   final Room room;
@@ -30,7 +28,7 @@ class _RoomDevicesTabState extends State<RoomDevicesTab> {
   String selectedDeviceType = 'Fan';
   TextEditingController deviceNameController = TextEditingController();
 
-  Future<void> _showAddDeviceDialog(BuildContext context, Room room) async {
+  Future<void> _showAddDeviceDialog(BuildContext context) async {
     return showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -141,8 +139,7 @@ class _RoomDevicesTabState extends State<RoomDevicesTab> {
                   .map(
                     (device) => DeviceTab(
                       device: device,
-                      onDeviceUpdated: (Device device, [String? newName]) {
-                        // * Missing case -- device name updated
+                      onDeviceUpdated: (Device device) {
                         setState(() {
                           widget.room.updateRoomDevice(device);
                         });
@@ -157,7 +154,7 @@ class _RoomDevicesTabState extends State<RoomDevicesTab> {
                   .toList(),
             ),
             GestureDetector(
-              onTap: () => _showAddDeviceDialog(context, widget.room),
+              onTap: () => _showAddDeviceDialog(context),
               child: const Row(
                 children: [
                   Icon(Icons.add_circle_outline),
@@ -169,135 +166,6 @@ class _RoomDevicesTabState extends State<RoomDevicesTab> {
                     style: TextStyle(
                       fontSize: 20,
                     ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class DeviceTab extends StatefulWidget {
-  final Device device;
-  final Function(Device, [String? newName]) onDeviceUpdated;
-  final Function(Device) onDeviceDeleted;
-
-  const DeviceTab({
-    super.key,
-    required this.device,
-    required this.onDeviceUpdated,
-    required this.onDeviceDeleted,
-  });
-
-  @override
-  State<DeviceTab> createState() => _DeviceTabState();
-}
-
-class _DeviceTabState extends State<DeviceTab> {
-  // Change status of device when switch is toggled
-  void updateSwitch(SwitchStatus status) {
-    setState(() {
-      widget.device.isOn = status == SwitchStatus.on;
-    });
-
-    // OR once the state changes, go to main dashboard and trigger a function that checks if all devices are off
-    // pass the change to main dashboard
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    String deviceIcon = widget.device is LightDevice
-        ? 'assets/icons/lightbulb.svg'
-        : 'assets/icons/fan.svg';
-
-    String deviceFeature =
-        widget.device is LightDevice ? 'Brightness' : 'Fan Speed';
-
-    double devicePercentage = widget.device.value;
-
-    return Container(
-      margin: const EdgeInsets.only(bottom: 24),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey),
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(8, 24, 32, 24),
-        child: Row(
-          children: [
-            DeviceSettingsMenu(
-              device: widget.device,
-              onDeviceUpdated: widget.onDeviceUpdated,
-              onDeviceDeleted: widget.onDeviceDeleted,
-            ),
-            Expanded(
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          SvgPicture.asset(
-                            deviceIcon,
-                            height: 40,
-                            width: 40,
-                          ),
-                          const SizedBox(
-                            width: 16,
-                          ),
-                          Text(
-                            widget.device.name,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 24,
-                            ),
-                          ),
-                        ],
-                      ),
-                      CustomSwitch(
-                        value: widget.device.isOn
-                            ? SwitchStatus.on
-                            : SwitchStatus.off,
-                        onChanged: updateSwitch,
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        deviceFeature,
-                        style: const TextStyle(fontSize: 16),
-                      ),
-                      Text(
-                        '${(devicePercentage * 100).round()}%',
-                        style: const TextStyle(fontSize: 16),
-                      ),
-                      // Text(widget.device is LightDevice ? widget.device. : )
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  Slider(
-                    value: widget.device.value,
-                    min: 0,
-                    max: 1,
-                    // TODO: Persist the new value, communicate with IoT device
-                    onChanged: (newValue) {
-                      setState(() {
-                        widget.device.setValue(newValue);
-                      });
-                    },
-                    activeColor: const Color(0xFF455A64),
-                    inactiveColor: const Color(0xFFCFD8DC),
                   ),
                 ],
               ),
