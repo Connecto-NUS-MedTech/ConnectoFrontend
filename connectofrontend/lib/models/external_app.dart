fc5c68d7ac7900fbc993452b0f39436d27b05706
ish1506 <ishiuanlee@gmail.com>
2024-02-05 22:34:11 +0800

    Extract external app-related widgets

---
+++ connectofrontend/lib/models/external_app.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ExternalApp {
+  final String name;
+  final IconData icon;
+
+  const ExternalApp({required this.name, required this.icon});
+}
--- connectofrontend/lib/screens/main_dashboard/main_dashboard.dart
+++ connectofrontend/lib/screens/main_dashboard/main_dashboard.dart
@@ -1,18 +1,11 @@
 import 'package:connectofrontend/models/room.dart';
 import 'package:connectofrontend/providers/home_system_state.dart';
+import 'package:connectofrontend/widgets/external_apps/external_apps_row.dart';
 import 'package:connectofrontend/widgets/home_system/home_env_control.dart';
 import 'package:connectofrontend/widgets/home_system/home_system_header_row.dart';
 import 'package:connectofrontend/widgets/home_system/home_system_rooms_tab.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:external_app_launcher/external_app_launcher.dart';
-
-class AppInfo {
-  final String name;
-  final IconData icon;
-
-  AppInfo({required this.name, required this.icon});
-}
 
 class MainDashboardScreen extends StatefulWidget {
   const MainDashboardScreen({super.key});
@@ -22,62 +15,6 @@ class MainDashboardScreen extends StatefulWidget {
 }
 
 class MainDashboardScreenState extends State<MainDashboardScreen> {
-
-  Widget buildExternalApplications() {
-    // Replace this with your actual list of apps
-    List<AppInfo> externalApps = [
-      AppInfo(name: 'Chrome', icon: Icons.apps),
-      AppInfo(name: 'Instagram', icon: Icons.apps),
-      AppInfo(name: 'Maps', icon: Icons.apps),
-      AppInfo(name: 'Add App', icon: Icons.add),
-      AppInfo(name: 'Add App', icon: Icons.add),
-
-      // Add more apps as needed
-    ];
-
-    return Container(
-      height: 125,
-      color: Colors.grey,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: externalApps.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Column(
-              children: [
-                IconButton(
-                  icon: Icon(
-                    externalApps[index].icon,
-                    size: 48, // Adjust the size of the icon
-                  ),
-                  onPressed: () async {
-                    await LaunchApp.openApp(
-                      androidPackageName: 'com.android.chrome',
-                      openStore: false,
-                    );
-                    // Handle app button click
-                    // You can navigate to the app or perform any other action
-                  },
-                ),
-                const SizedBox(
-                  height: 8, // Add spacing between the icon and text
-                ),
-                Text(
-                  externalApps[index].name,
-                  style: const TextStyle(
-                    fontSize: 16, // Adjust the size of the text
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-          );
-        },
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     List<Room> bookmarkedRooms =
@@ -91,8 +28,7 @@ class MainDashboardScreenState extends State<MainDashboardScreen> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                // Replace the Container with custom widget
-                buildExternalApplications(),
+                const ExternalApplicationsRow(),
                 const SizedBox(height: 25),
                 // Home Environmental Conditions Control section
                 HomeEnvironmentalConditionsControlTab(
